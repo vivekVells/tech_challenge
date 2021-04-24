1c2d2e5e6ab744bfedae36206fa96533031ea44c
Vivek Vellaiyappan Surulimuthu <vivekvellaiyappans@gmail.com>
2021-04-23 23:50:23 -0500

    frontend - working code.

---
+++ iheart-songs/src/graphql/queries.ts
@@ -0,0 +1,31 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+import { gql } from 'apollo-boost'
+
+export const GET_SONGS = gql`
+  query GetSongs {
+    getSongs {
+      song
+      artist
+      songReleaseDate
+      playCount
+      metricA
+      metricB
+      metricC
+      metricD
+      metricE
+      metricF
+      metricG
+      metricH
+      metricI
+      metricJ
+      metricK
+      metricL
+      metricM
+      metricN
+      metricO
+      metricP
+    }
+  }
+`;
