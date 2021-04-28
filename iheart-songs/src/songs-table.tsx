050c36323d8997bf0643d1baa9c0230e2397e761
Vivek Vellaiyappan Surulimuthu <vivekvellaiyappans@gmail.com>
2021-04-27 19:07:17 -0500

    frontend - implemented the sort functionality for the columns fields

--- iheart-songs/src/songs-table.tsx
+++ iheart-songs/src/songs-table.tsx
@@ -1,11 +1,39 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import "./songs-table.css";
 import useSongs from "./hooks/use-songs";
 import { useHistory } from "react-router";
+import { Song } from "./API";
 
 const SongsTable: React.FC = () => {
   const [loading, _, songs] = useSongs();
-  
+  const [songsData, setSongsData] = useState<Song[]>(songs || []);
+  const [sortAscending, setSortAscending] = useState<boolean>(true);
+  const [sortedColumnHeader, setSortedColumnHeader] = useState("");
+  const UP_ARROW = "🔼";
+  const DOWN_ARROW = "🔽";
+  const columnHeaderSongMap = {
+    "Song Name": "song",
+    Artist: "artist",
+    "Release Date": "songReleaseDate",
+    "Play Count": "playCount",
+    "Metrics A": "metricA",
+    "Metrics B": "metricB",
+    "Metrics C": "metricC",
+    "Metrics D": "metricD",
+    "Metrics E": "metricE",
+    "Metrics F": "metricF",
+    "Metrics G": "metricG",
+    "Metrics H": "metricH",
+    "Metrics I": "metricI",
+    "Metrics J": "metricJ",
+    "Metrics K": "metricK",
+    "Metrics L": "metricL",
+    "Metrics M": "metricM",
+    "Metrics N": "metricN",
+    "Metrics O": "metricO",
+    "Metrics P": "metricP",
+  };
+
   // to generate metrics header value from 'Metrics A' to 'Metrics P'
   const metricsHeader = useMemo(() => {
     let charCount = 65;
@@ -14,7 +42,6 @@ const SongsTable: React.FC = () => {
       .fill("")
       .map(() => `Metrics ${String.fromCharCode(charCount++)}`);
   }, []);
-
   const tableHeaders = [
     "Artist",
     "Song Name",
@@ -23,7 +50,43 @@ const SongsTable: React.FC = () => {
     ...metricsHeader,
   ];
 
-  console.log({ ge: metricsHeader });
+  useEffect(() => {
+    songs && setSongsData(songs);
+  }, [songs]);
+
+  const handleSorting = (columnName: string, orderType: boolean) => {
+    return [...songsData].sort((songAData, songBData) => {
+      // @ts-ignore
+      const columnAValue = songAData[columnName];
+      // @ts-ignore
+      const columnBValue = songBData[columnName];
+
+      // @ts-ignore
+      if (columnAValue && columnBValue) {
+        if (columnName === "songReleaseDate") {
+          const dates = [new Date(columnAValue), new Date(columnBValue)];
+
+          if (orderType) {
+            // @ts-ignore
+            return dates[0] - dates[1];
+          } else {
+            // @ts-ignore
+            return dates[1] - dates[0];
+          }
+        }
+
+        if (orderType) {
+          // @ts-ignore
+          return columnAValue < columnBValue ? -1 : 1;
+        } else {
+          // @ts-ignore
+          return columnAValue > columnBValue ? -1 : 1;
+        }
+      }
+
+      return 0;
+    });
+  };
 
   const Table: React.FC = () => {
     const history = useHistory();
@@ -37,15 +100,32 @@ const SongsTable: React.FC = () => {
                 className="attribute attribute-header"
                 data-name={header}
                 onClick={() => {
-                  console.log("clicked", header);
+                  // @ts-ignore
+                  const sortedSongsData = handleSorting(
+                    // @ts-ignore
+                    columnHeaderSongMap[header],
+                    sortAscending
+                  );
+
+                  setSongsData(sortedSongsData);
+                  setSortAscending(!sortAscending);
+                  setSortedColumnHeader(header);
                 }}
               >
-                {header}
+                <span>{header}</span>
+                <span>
+                  {sortedColumnHeader === header && sortAscending && UP_ARROW}
+                </span>
+                <span>
+                  {sortedColumnHeader === header &&
+                    !sortAscending &&
+                    DOWN_ARROW}
+                </span>
               </div>
             ))}
           </li>
 
-          {songs?.map(songData => (
+          {songsData?.map((songData) => (
             <li
               className="item item-container"
               onClick={() => {
@@ -79,38 +159,38 @@ const SongsTable: React.FC = () => {
               <div className="attribute" data-name={songData.metricE}>
                 {songData.metricE}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricF}>
+                {songData.metricF}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricG}>
+                {songData.metricG}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricH}>
+                {songData.metricH}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricI}>
+                {songData.metricI}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricJ}>
+                {songData.metricJ}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricK}>
+                {songData.metricK}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricL}>
+                {songData.metricL}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricM}>
+                {songData.metricM}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricN}>
+                {songData.metricN}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricO}>
+                {songData.metricO}
               </div>
-              <div className="attribute" data-name={songData.metricE}>
-                {songData.metricE}
+              <div className="attribute" data-name={songData.metricP}>
+                {songData.metricP}
               </div>
             </li>
           ))}
